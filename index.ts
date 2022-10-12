b4b59c2e8e1e15f40ab01dab5c068c35881234fa
PanosKontos <panos96el@gmail.com>
2022-10-12 21:49:41 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as eks from "@pulumi/eks";
 
 
 var buckets = ["pulumi-1","pulumi-2","pulumi-3"]
@@ -12,5 +13,20 @@ buckets.map((name)=>{
     // Export the name of the buckets
     mynames.push(bucket.id);
 })
-
 exports.bucketName = mynames
+
+
+// Create VPC
+const vpc = new awsx.ec2.Vpc("vpc",{
+    cidrBlock: "10.0.0.0/16"
+})
+
+// Cluster
+const cluster = new eks.Cluster("cluster", {
+    vpcId: vpc.id,
+    subnetIds:vpc.publicSubnetIds,
+    instanceType: "t2.medium",
+})
+
+exports.kubeconfig = cluster.kubeconfig
+
