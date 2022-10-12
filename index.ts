0e9806b0c790f01c5a84ba63a745f7e1fad615c9
PanosKontos <panos96el@gmail.com>
2022-10-12 21:22:37 +0300

    first commit

--- index.ts
+++ index.ts
@@ -2,8 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-pulumi-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+var buckets = ["pulumi-1","pulumi-2","pulumi-3"]
+var mynames:any[] = []
+
+// Create an AWS resource (S3 Bucket)
+buckets.map((name)=>{
+    const bucket = new aws.s3.Bucket(name);
+    // Export the name of the bucket
+    mynames.push(bucket.id);
+})
+
+exports.bucketName = mynames
